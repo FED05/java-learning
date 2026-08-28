84c1e985ade021e57196c0d7a77383b1ceb3357a
FED05 <fatimaezzahra.dannouche@gmail.com>
2026-08-28 20:41:59 +0100

    caculate the averge on 3 notes with return methode

--- src/CalculateAverage.java
+++ src/CalculateAverage.java
@@ -1,2 +1,9 @@
 public class CalculateAverage {
+    public static double calculateAverage(double n,double m,double q){
+        return (n+m+q)/3;
+    }
+    public static void main(String[] args){
+        double avg = calculateAverage(15.4,6.6,17.8);
+        System.out.println("the avg :" + avg);
+    }
 }
